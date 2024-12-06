893fee0df24521f598dc7861057bcd903851ca91
B612-jujin <jujingood@gmail.com>
2024-12-06 12:25:21 +0900

    리액트 기초

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from "./jujingood/Home";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
+function App() {
+  return (
+      <Router>
+        <Routes>
+            <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+  );
+}
+
+export default App;
